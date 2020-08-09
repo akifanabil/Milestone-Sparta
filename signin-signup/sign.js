900c924d921399f5dfea78a5ef60ae1188ff38b3
Akifa Nabil <68159046+akifanabil@users.noreply.github.com>
2020-08-09 20:24:17 +0700

    nambah sistem validasi+kehubung ke main menu

--- signin-signup/sign.js
+++ signin-signup/sign.js
@@ -9,3 +9,37 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
+
+// click and validate login button
+
+
+var uname=document.getElementById("uname-login");
+var passw=document.getElementById("password-login")
+
+function klik1(){
+  if (!uname.checkValidity()||!passw.checkValidity()){
+    document.getElementById("demo").innerHTML="You entered wrong username or password.";
+  } else{
+    document.location.href="../menu/menu(loggedin).html";
+  }
+}
+
+//click and validate signup button
+
+var un=document.getElementById("uname-signup")
+var email=document.getElementById("email-signup")
+var pw=document.getElementById("passw-signup")
+
+function klik2(){
+  if (!un.checkValidity()||!pw.checkValidity() || !email.checkValidity()){
+    document.getElementById("wrong").innerHTML="Invalid input.";
+  } else{
+    document.location.href="../menu/menu(loggedin).html";
+  }
+}
+
+// if uname{
+//   const name=uname;
+// } else{
+//   const name=un
+// }
