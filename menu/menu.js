f1bcd7525564e8de9ac315f6084f1360f8a3734b
Akifa Nabil <68159046+akifanabil@users.noreply.github.com>
2020-08-09 20:25:05 +0700

    nambah page menu

---
+++ menu/menu.js
@@ -0,0 +1,21 @@
+// import {name} from "../signin-signup/sign.js";
+
+$(document).ready(function() {
+    $(".menu-icon").on("click", function() {
+          $("nav ul").toggleClass("showing");
+    });
+});
+
+// Scrolling Effect
+
+$(window).on("scroll", function() {
+    if($(window).scrollTop()) {
+          $('nav').addClass('black');
+    }
+
+    else {
+          $('nav').removeClass('black');
+    }
+})
+
+// document.getElementById("uname").innerHTML=name;
